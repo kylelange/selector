fdb0baecd0148b8a6e9eecff5aaf56b70d481d7b
Kyle Lange <baronsintrees@gmail.com>
2016-08-12 13:52:21 -0700

    added the final two questions.  Javascript looks complete, but not working yet...

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,36 @@
 // User Interface Logic
 
 $(document).ready(function(){
-  $(form).submit(function(event){
+  $("form").submit(function(event){
+    var q1 = $("#q1").val();
+    var q2 = $("#q2").val();
+    var q3 = $("#q3").val();
+    // var q4 = $("#q4").val();
+    // var q5 = $("#q5").val();
 
-  // var q1 = $("#q1").val();
-  // var q2 = $("#q2").val();
-  // var q3 = $("#q3").val();
-  // var q4 = $("#q4").val();
-  // var q5 = $("#q5").val();
+    // $("#css-d").show();
+    // alert("hello");
 
-  $("#css-d").show();
-
-  event.preventdefault();
+    if (q1 === "1-yes" && q2 === "2-yes" && q3 === "3-yes"){
+      alert("hello");
+      $("#css-d").hide();
+      $("#php-ruby").hide();
+      $("#c-java").show();
+    }
+      else if (q1 === "1-no" && q2 === "2-no" && q3 === "3-no"){
+        alert("hello");
+        $("#php-ruby").hide();
+        $("#c-java").hide();
+        $("#css-d").show();
+      }
+      else {
+        // alert("hello");
+        $("#c-java").hide();
+        $("#css-d").hide();
+        $("#php-ruby").show();
+      }
+    event.preventdefault();
+    // alert("hello");
+    console.log("Program Complete!");
   });
 });
-
-console.log("Program Complete!");
