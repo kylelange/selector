e7dc66b8deb99856c8374a19a892cc8215ab869d
Kyle Lange <baronsintrees@gmail.com>
2016-08-12 15:26:25 -0700

    add further html placement for button and h3. v0.3

--- js/scripts.js
+++ js/scripts.js
@@ -13,35 +13,25 @@ $(document).ready(function(){
     // var q4 = $("#q4").val();
     // var q5 = $("#q5").val();
 
-
-
-
     if (q1 === "1-yes" && q2 === "2-yes" && q3 === "3-yes"){
       alert("hello1");
       $("#css-d").hide();
       $("#php-ruby").hide();
       $("#c-java").show();
-      // event.preventdefault();
     }
       else if (q1 === "1-no" && q2 === "2-no" && q3 === "3-no"){
         alert("hello2");
         $("#php-ruby").hide();
         $("#c-java").hide();
         $("#css-d").show();
-        // event.preventdefault();
       }
       else {
         alert("hello3");
         $("#c-java").hide();
         $("#css-d").hide();
         $("#php-ruby").show();
-        // event.preventdefault();
       }
-
     event.preventdefault();
-
-
   });
 });
-
 console.log("Program Complete!");
