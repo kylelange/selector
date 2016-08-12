ae84128a57a201ec719d6f3ba6d9f23b11335960
Kyle Lange <baronsintrees@gmail.com>
2016-08-12 12:50:11 -0700

    add html dropdown text v0.2

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,13 @@
 $(document).ready(function(){
   $(form).submit(function(event){
 
-  var q1 = $("#q1").val();
-  var q2 = $("#q2").val();
-  var q3 = $("#q3").val();
+  // var q1 = $("#q1").val();
+  // var q2 = $("#q2").val();
+  // var q3 = $("#q3").val();
   // var q4 = $("#q4").val();
   // var q5 = $("#q5").val();
+
+  $("#css-d").show();
 
   event.preventdefault();
   });
